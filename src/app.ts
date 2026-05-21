efb72e684ec38cb565a4d0526ca3166a257eada9
zzzJe <kevin0966698073@gmail.com>
2026-05-22 00:40:28 +0800

    chore: make type error

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
+const imNumber: number = "sike";
+console.log(imNumber);
+
 export function buildApp(options: FastifyServerOptions = {}) {
   const app = Fastify({
     logger: options.logger ?? true,
@@ -9,7 +12,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
-      version: process.env.APP_VERSION || 'dev',
+      version: process.env.APP_VERSION || 'dev'
     };
   });
 
