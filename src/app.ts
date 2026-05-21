4002835912d181f1a3d5dd846863022f2f5637d9
zzzJe <kevin0966698073@gmail.com>
2026-05-22 00:29:22 +0800

    chore: make type error

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
-      version: process.env.APP_VERSION || 'dev'
+      version: process.env.APP_VERSION || 'dev',
     };
   });
 
